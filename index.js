c21953bd7fc6a54fa249fe41cd8e248e7455fe69
MutaiAllan <MutaiAllan>
2023-07-03 15:31:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1;j < array.length; j++) {
+        sum = array[i] + array[j]
+        if (sum == target) {
+          return true
+        }
+    }
+  }
+  return false
+  
 }
 
 /* 
